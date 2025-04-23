6345a05f913a53d119842d112c79d95246f1c505
Muhammad Abubakar <163642750+sheikh-abubakar@users.noreply.github.com>
2025-04-23 18:30:14 +0500

    front pages done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,6 +38,7 @@ const App = () => {
       <Navbar />
 
       <Routes>
+        
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "axios";//Global State management library
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
