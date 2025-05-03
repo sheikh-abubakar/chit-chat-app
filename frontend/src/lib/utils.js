2dfe05c6ff341946c21a8a251fe7582766a61940
Muhammad Abubakar <163642750+sheikh-abubakar@users.noreply.github.com>
2025-05-03 16:07:19 +0500

    time formatted chanaged

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -2,6 +2,6 @@ export function formatMessageTime(date) {
   return new Date(date).toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
-    hour12: false,
+    hour12: true,
   });
 }
