7b290ab7494f98d5f94c0e102e091a2eb85cb4e3
Muhammad Abubakar <163642750+sheikh-abubakar@users.noreply.github.com>
2025-05-18 17:15:07 +0500

    admin list updated

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -2,8 +2,13 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 
 export const getAllUsers = async (req, res) => {
-  const users = await User.find().select("-password");
-  res.json(users);
+  try {
+    // Exclude the admin user (the one making the request)
+    const users = await User.find({ _id: { $ne: req.user._id } }).select("-password");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
 };
 
 export const blockUser = async (req, res) => {
